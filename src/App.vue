d996eb8bc13119d43763cd4ee5f0a46d13d1ca40
silviosalza <silviofilipposalza@gmail.com>
2023-04-12 14:24:07 +0200

    push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppBanner from "./components/AppBanner.vue"
+export default{
+  components:{
+      AppBanner
+    },
+  data(){
+   
+    return{
+      title: "MY TITLE"
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Hello vue with {{ title }}</h1>
+  <AppBanner/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+h1{
+  color: red;
 }
 </style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+    name: "AppBanner",
+    data(){
+        return{
+            text: "lorem ipsum"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Banner</h2>
+    <p>{{ text }}</p>
+</template>
+
+
+
+<style>
+</style>
